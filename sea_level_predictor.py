8e9d00209a58fbd2bb2a1d157b24cda08ee0e885
Josh Earl Conrad Paclibar <Joshearlconradpaclibar@gmail.com>
2025-05-01 06:12:14 +0000

    1 error

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,19 +4,40 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv('epa-sea-level.csv')
 
     # Create scatter plot
+    x = df['Year']
+    y = df['CSIRO Adjusted Sea Level']
 
+    fig, ax = plt.subplots(figsize=(12,12))
+
+    ax = plt.scatter(x,y)
 
     # Create first line of best fit
+    reg = linregress(x,y)
+    print(reg)
+    x_forecast = pd.Series(([i for i in range(1850,2051)]))
+    y_forecast = reg.slope * x_forecast + reg.intercept
+    plt.plot(x_forecast, y_forecast, 'r-')
 
+    df_forecast = df.loc[df['Year'] >= 2000]
+    new_x = df_forecast['Year']
+    new_y = df_forecast['CSIRO Adjusted Sea Level']
 
     # Create second line of best fit
-
+    new_reg = linregress(new_x, new_y)
+    new_x_forecast = pd.Series(([i for i in range(2000,2051)]))
+    new_y_forecast = new_reg.slope * new_x_forecast + new_reg.intercept
+    plt.plot(new_x_forecast, new_y_forecast, 'orange')
 
     # Add labels and title
+    plt.title("Rise in Sea Level")
+    plt.xlabel("Year")
+    plt.ylabel("Sea Level (inches)")
 
+    x_ticks = [1850, 1875, 1900, 1925, 1950, 1975, 2000, 2025, 2050, 2075]
+    plt.xticks(x_ticks)
     
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
